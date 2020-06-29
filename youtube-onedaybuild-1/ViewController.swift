49faaafddc7526a27245d3eb36ea0f674a2b8f28
Kuzin4U <sergey.kuzin@list.ru>
2020-06-29 15:00:48 +0300

    initial xcode project creation

---
+++ youtube-onedaybuild-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild-1
+//
+//  Created by Сергей Кузин on 28.06.2020.
+//  Copyright © 2020 Сергей Кузин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
